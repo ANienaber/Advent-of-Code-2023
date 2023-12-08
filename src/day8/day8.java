d835d68a397f13cc170293b69591e8e7601581ec
Anna <anna.nienaber@uol.de>
2023-12-08 15:03:52 +0100

    day 8 partial

--- src/day8/day8.java
+++ src/day8/day8.java
@@ -1,28 +1,55 @@
 package day8;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
 import utils.utils;
-import day8.Node;
 
 public class day8 {
     public static void main(String[] args) throws FileNotFoundException {
-        List<String> lines = utils.readFile("src\\day8\\test.txt");
-        String direction = lines.get(0);
-        List<Node> nodes = new ArrayList<>();
-        lines.remove(0);
-        lines.remove(1);
+        List<String> lines = utils.readFile("src\\day8\\input.txt");
+        String directions = lines.get(0);
+        String[][] nodes = new String[lines.size()][3];
+        //lines.removeFirst(); lines.removeFirst(); lines.removeFirst();
 
-        for (String line : lines) {
-            String thisNode = line.split(" = ")[0];
-            String otherNodes = line.split(" = ")[1].split("(")[1].split(")")[0];
+
+        for (int i = 2; i < lines.size(); i++) {
+            String thisNode = lines.get(i).split(" = ")[0];
+            String otherNodes = lines.get(i).split(" = ")[1].split("\\(")[1].split("\\)")[0];
             String leftNode = otherNodes.split(", ")[0];
             String rightNode = otherNodes.split(", ")[1];
 
-            if (nodes.contains(new Node(thisNode))){
-
-            }
+            nodes[i-2][0] = thisNode;
+            nodes[i-2][1] = leftNode;
+            nodes[i-2][2] = rightNode;
         }
+
+        int steps = 0;
+        int direction = 0;
+        int i = 0;
+        int j = 0;
+        while (!nodes[i][0].equals("ZZZ")){
+            String[] currentNode = nodes[i];
+            char currDir = directions.charAt(direction);
+            if (currDir == 'L'){
+                String destination = currentNode[1];
+                for (j = 0; j < nodes.length; j++) {
+                    if(nodes[j][0].equals(destination)){
+                        break;
+                    }
+                }
+            }else if (currDir == 'R'){
+                String destination = currentNode[2];
+                for (j = 0; j < nodes.length; j++) {
+                    if(nodes[j][0].equals(destination)){
+                        break;
+                    }
+                }
+            }
+            i = j;
+            direction = (direction + 1) % 3;
+            steps++;
+        }
+        System.out.println("Number of Steps taken: "+steps);
     }
 }
