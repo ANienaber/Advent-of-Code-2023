dc475879ee70dc4d08ea7fb1ec4f1fec72482024
Anna Nienaber <anna.nienaber@uni-oldenburg.de>
2023-12-13 19:08:54 +0100

    day 8

---
+++ src/day13/day13.java
@@ -0,0 +1,170 @@
+package day13;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class day13 {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("Puzzle 1:-----------------------------------");
+        solve1("src/day13/test.txt");
+        System.out.println("Puzzle 2:-----------------------------------");
+        solve2("src/day13/input.txt");
+    }
+
+    private static void solve1(String path) throws FileNotFoundException {
+        List<List<String>> sets = getSets(path);
+        int sum = 0;
+        for (List<String> set : sets) {
+            System.out.println("Set: ");
+            for ( String line : set) {
+                System.out.println(line);
+            }
+            int setSum = getSum(set);
+            System.out.println("Sum for this set is: "+setSum);
+            sum += setSum;
+        }
+        System.out.println("\nOverall sum is: "+sum);
+    }
+
+    private static int getSum(List<String> set) {
+        int rowSum = getRowMirror(set, false);
+        if(rowSum > 0){
+            return rowSum;
+        } else {
+            return getColumnMirror(set, false);
+        }
+    }
+
+    private static void solve2(String path) throws FileNotFoundException {
+        List<List<String>> sets = getSets(path);
+        int sum = 0;
+        for (List<String> set : sets) {
+            System.out.println("Set: ");
+            for (String line : set) {
+                System.out.println(line);
+            }
+            int setSum = getSmudgeSum(set);
+            System.out.println("Sum for this set is: "+setSum);
+            sum += setSum;
+        }
+        System.out.println("\nOverall sum is: "+sum);
+    }
+
+    private static int getSmudgeSum(List<String> set) {
+        int rowSum = getRowMirror(set, true);
+        if(rowSum > 0){
+            return rowSum;
+        } else {
+            return getColumnMirror(set, true);
+        }
+    }
+
+
+
+    private static int getRowMirror(List<String> set, boolean joker) {
+        boolean startJoker = joker;
+        boolean found = false;
+        int mirrorIdx = 0;
+        for (int i = mirrorIdx; i < set.size() - 1; i++) {
+            joker = startJoker;
+            if(rowMatches(set, i, i+1, joker)){
+                found = true;
+                mirrorIdx = i;
+                //check other line
+                for (int j = 1; j < set.size()/2 ; j++) {
+                    // mirrorIdx +j +1 bc mirrorIdx is first of the two mirrored lines
+                    int rightIdx = mirrorIdx + j + 1;
+                    int leftIdx = mirrorIdx - j;
+                    if(leftIdx >= 0 && rightIdx < set.size()
+                            && !(rowMatches(set, leftIdx, rightIdx, joker))){
+                        found = false;
+                        break;
+                    }
+                }
+            }
+            if (found){
+                System.out.println("Mirror between row "+(mirrorIdx+1)+" and "+(mirrorIdx+2));
+                return 100 * (mirrorIdx +1);
+            }
+        }
+        System.out.println("Row not found");
+        return 0;
+    }
+
+    private static boolean rowMatches(List<String> set, int i1, int i2, boolean joker) {
+        String row1 = set.get(i1);
+        String row2 = set.get(i2);
+        for (int i = 0; i < row1.length(); i++) {
+            if (row1.charAt(i) != row2.charAt(i)) {
+                if (joker){
+                    joker = false;
+                    System.out.println("Smudge has been fixed in row "+(i1+1)+" / "+(i2+1)+" and col "+(i+1));
+                } else return false;
+            }
+        }
+        return true;
+    }
+
+    private static int getColumnMirror(List<String> set, boolean joker) {
+        boolean startJoker = joker;
+        boolean found = false;
+        int mirrorCol = 0;
+        int lineLength = set.get(1).length();
+        for (int i = mirrorCol; i < lineLength - 1; i++) {
+            joker = startJoker;
+            //if column matches
+            if (colMatches(set, i, i + 1, joker)) {
+                found = true;
+                mirrorCol = i;
+                //check other columns
+                for (int j = 1; j < lineLength / 2; j++) {
+                    // mirrorIdx +j +1 bc mirrorIdx is first of the two mirrored lines
+                    int rightCol = mirrorCol + j + 1;
+                    int leftCol = mirrorCol - j;
+                    if (leftCol >= 0 && rightCol < lineLength
+                            && !(colMatches(set, leftCol, rightCol, joker))) {
+                        found = false;
+                        break;
+                    }
+                }
+            }
+
+            if (found) {
+                System.out.println("Mirror between col " + (mirrorCol+1) + " and " + (mirrorCol + 2));
+                return mirrorCol + 1;
+            }
+        }
+        System.out.println("Col not found");
+        return 0;
+    }
+
+
+
+    private static boolean colMatches(List<String> set, int col1, int col2, boolean joker) {
+        int i = 0;
+        for (String s : set) {
+            if (s.charAt(col1) != s.charAt(col2)) {
+                if (joker){
+                    joker = false;
+                    System.out.println("Smudge has been fixed in col "+(col1+1)+" / "+(col2+1)+" and col "+(i+1));
+                } else return false;
+            }
+            i++;
+        }
+        return true;
+    }
+
+    private static List<List<String>> getSets(String path) throws FileNotFoundException {
+        List<String> lines = utils.utils.readFile(path);
+        List<List<String>> sets = new ArrayList<>();
+        int startIdx = 0;
+        for (int i = 0; i < lines.size(); i++) {
+            if (lines.get(i).equals("") || i == lines.size() - 1){
+                sets.add(new ArrayList<>(lines.subList(startIdx, i)));
+                startIdx = i+1;
+            }
+        }
+        return sets;
+    }
+}
--- src/day8/day8.java
+++ src/day8/day8.java
@@ -3,13 +3,14 @@ package day8;
 import java.io.FileNotFoundException;
 import java.util.*;
 
+import groovy.lang.GString;
 import utils.utils;
 
 public class day8 {
     public static void main(String[] args) throws FileNotFoundException {
         List<String> lines = utils.readFile("src\\day8\\input.txt");
         String directions = lines.get(0);
-        String[][] nodes = new String[lines.size()][3];
+        String[][] nodes = new String[lines.size()-2][5];
         //lines.removeFirst(); lines.removeFirst(); lines.removeFirst();
 
 
@@ -24,32 +25,76 @@ public class day8 {
             nodes[i-2][2] = rightNode;
         }
 
-        int steps = 0;
+
+
+
+        long steps = 0;
         int direction = 0;
         int i = 0;
         int j = 0;
-        while (!nodes[i][0].equals("ZZZ")){
-            String[] currentNode = nodes[i];
-            char currDir = directions.charAt(direction);
-            if (currDir == 'L'){
-                String destination = currentNode[1];
-                for (j = 0; j < nodes.length; j++) {
-                    if(nodes[j][0].equals(destination)){
-                        break;
-                    }
-                }
-            }else if (currDir == 'R'){
-                String destination = currentNode[2];
-                for (j = 0; j < nodes.length; j++) {
-                    if(nodes[j][0].equals(destination)){
-                        break;
-                    }
-                }
+        List<String[]> currentNodes = new ArrayList<>();
+        List<String[]> nextNodes = new ArrayList<>();
+
+        //find AAA for start
+        for (i = 0; i < nodes.length ; i++) {
+            if(nodes[i][0].charAt(2) == 'A'){
+                currentNodes.add(nodes[i]);
             }
-            i = j;
-            direction = (direction + 1) % 3;
+        }
+        int startNodeCount = currentNodes.size();
+
+
+        while (!checkNodesForZ(currentNodes)){
+            //System.out.println(nodes[i][0] + " " + nodes[i][1] + " " + nodes[i][2] + " " + nodes[i][3] +" "+nodes[i][4] +" Direction: "+directions.charAt(direction));
+            for (String[] currentNode: currentNodes) {
+                String nodeLabel = currentNode[0];
+                char currDir = directions.charAt(direction);
+                if (currDir == 'L') {
+                    if (currentNode[3] != null) {
+                        j = Integer.parseInt(currentNode[3]);
+                    } else {
+                        String destination = currentNode[1];
+                        for (j = 0; j < nodes.length; j++) {
+                            if (nodes[j][0].equals(destination)) {
+                                break;
+                            }
+                        }
+                        currentNode[3] = String.valueOf(j);
+                    }
+                } else if (currDir == 'R') {
+                    if (currentNode[4] != null) {
+                        j = Integer.parseInt(currentNode[4]);
+                    } else {
+                        String destination = currentNode[2];
+                        for (j = 0; j < nodes.length; j++) {
+                            if (nodes[j][0].equals(destination)) {
+                                break;
+                            }
+                        }
+                        currentNode[4] = String.valueOf(j);
+                    }
+                }
+                //System.out.print("Destination: "+nodes[j][0]+" ");
+                //System.out.println("Went "+directions.charAt(direction)+ " at index " + direction +" with "+steps+" steps");
+                nextNodes.add(nodes[j]);
+            }
+            direction = (direction + 1) % directions.length();
             steps++;
+            System.out.println(steps);
+            currentNodes.clear();
+            currentNodes.addAll(nextNodes);
+            nextNodes.clear();
+            //System.out.println(steps);
         }
         System.out.println("Number of Steps taken: "+steps);
+    }
+
+    private static boolean checkNodesForZ(List<String[]> currentNodes) {
+        for (String[] node : currentNodes ) {
+            if (node[0].charAt(2) != 'Z'){
+                return false;
+            }
+        }
+        return true;
     }
 }
