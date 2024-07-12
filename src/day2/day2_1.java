28b499f88081dd43f91ad8931e26817c960ce00b
Anna <anna.nienaber@uol.de>
2024-07-12 12:06:15 +0200

    test

--- src/day2/day2_1.java
+++ src/day2/day2_1.java
@@ -45,7 +45,7 @@ In the example above, games 1, 2, and 5 would have been possible if the bag had 
 * 15 blue cubes at once. If you add up the IDs of the games that would have been possible, you get 8.
 
 Determine which games would have been possible if the bag had been loaded with only 12 red cubes,
-* 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
+* 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games? test
  */
 
 public class day2_1 {
